8514c7f71a3b0785217051eaeb9cd05742380684
Oscar Barragan <49173509+odbarragan@users.noreply.github.com>
2021-06-29 11:54:00 -0700

    fix bug

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -186,7 +186,7 @@ namespace WindowsFormsApp2
 
             // Create and add an inline picture with GIF image.
 
-            int[] arr = new int[] {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30};
+            int[] arr = new int[] {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80};
             for (int i = 0; i < urls_save.Count; i++)
             {
                 // Create first picture from resource data.
